b59e656c8b01c0b70f07f496d64d268b27358240
Gabriel Sena <gabrielssena.dev@gmail.com>
2026-02-26 22:18:23 -0300

    Começando a desenhar a calculadora

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,54 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+
+  const styles = StyleSheet.create({
+    row: {
+      flexDirection: 'row',
+    }
+  })
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View>
+      <View>
+        <Text>Calculadora: </Text>
+      </View>
+
+      <View style={styles.row}>
+        <Button title="C" />
+        <Button title="+-" />
+        <Button title="%" />
+        <Button title="÷" />
+      </View>
+
+      <View style={styles.row}>
+        <Button title="7" />
+        <Button title="8 " />
+        <Button title="9" />
+        <Button title="X" />
+      </View>
+
+      <View style={styles.row}>
+        <Button title="4" />
+        <Button title="5" />
+        <Button title="6" />
+        <Button title="-" />
+      </View>
+
+      <View style={styles.row}>
+        <Button title="1" />
+        <Button title="2" />
+        <Button title="3" />
+        <Button title="+" />
+      </View>
+
+      <View style={styles.row}>
+        <Button title="." />
+        <Button title="0" />
+        <Button title="⌫" />
+        <Button title="=" />
+      </View> 
+    </View>  
   );
 }
