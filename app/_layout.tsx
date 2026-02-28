85130817bbb011ea2017792c2a29ba50f217996a
Gabriel Sena <gabrielssena.dev@gmail.com>
2026-02-28 17:48:20 -0300

    ajustando layout dos botões da calculadora

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,54 +1,124 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
+function ButtonCalc({ title }) {
+  return (
+    <TouchableOpacity style={[styles.button]}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+function ButtonBlue({ title }) {
+  return (
+    <TouchableOpacity style={[styles.button, styles.blue]}>
+      <Text style={styles.textwhite}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+function ButtonGray({ title }) {
+  return (
+    <TouchableOpacity style={[styles.button, styles.gray]}>
+      <Text style={styles.textwhite}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
 
 
 export default function RootLayout() {
-
-  const styles = StyleSheet.create({
-    row: {
-      flexDirection: 'row',
-    }
-  })
-
   return (
-    <View>
+    <View style={styles.main}>
+
       <View>
-        <Text>Calculadora: </Text>
+        <Text>Resultado:</Text>
       </View>
 
-      <View style={styles.row}>
-        <Button title="C" />
-        <Button title="+-" />
-        <Button title="%" />
-        <Button title="÷" />
-      </View>
+      <View style={styles.content}>
+        <View style={styles.row}>
+          <ButtonGray title="C" type="dark" />
+          <ButtonGray title="+-" type="dark" />
+          <ButtonGray title="%" type="dark" />
+          <ButtonBlue title="÷" />
+        </View>
 
-      <View style={styles.row}>
-        <Button title="7" />
-        <Button title="8 " />
-        <Button title="9" />
-        <Button title="X" />
-      </View>
+        <View style={styles.row}>
+          <ButtonCalc title="7" />
+          <ButtonCalc title="8" />
+          <ButtonCalc title="9" />
+          <ButtonBlue title="X" />
+        </View>
 
-      <View style={styles.row}>
-        <Button title="4" />
-        <Button title="5" />
-        <Button title="6" />
-        <Button title="-" />
-      </View>
+        <View style={styles.row}>
+          <ButtonCalc title="4" />
+          <ButtonCalc title="5" />
+          <ButtonCalc title="6" />
+          <ButtonBlue title="-" />
+        </View>
 
-      <View style={styles.row}>
-        <Button title="1" />
-        <Button title="2" />
-        <Button title="3" />
-        <Button title="+" />
-      </View>
+        <View style={styles.row}>
+          <ButtonCalc title="1" />
+          <ButtonCalc title="2" />
+          <ButtonCalc title="3" />
+          <ButtonBlue title="+" />
+        </View>
 
-      <View style={styles.row}>
-        <Button title="." />
-        <Button title="0" />
-        <Button title="⌫" />
-        <Button title="=" />
-      </View> 
-    </View>  
+        <View style={styles.row}>
+          <ButtonCalc title="." />
+          <ButtonCalc title="0" />
+          <ButtonCalc title="⌫" />
+          <ButtonBlue title="=" />
+        </View>
+      </View>
+    </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    minHeight: "50vh",
+    alignItems: 'center',
+    backgroundColor: "#EDEFEF",
+  },
+
+  content: {
+    margin: 55
+  },
+
+  row: {
+    flexDirection: 'row',
+    margin: 5,
+    gap: 10,
+  },
+
+  button: {
+    width: 70,
+    height: 70,
+    borderRadius: 20,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  gray: {
+    backgroundColor: "#444550",
+  },
+
+  blue: {
+    backgroundColor: "#4448F4",
+  },
+
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "black",
+  },
+    textwhite: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  }
+});
