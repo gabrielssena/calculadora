bd452570d556e02c0621b2dbcea638f0911d0c51
Gabriel Sena <gabrielssena.dev@gmail.com>
2026-03-03 20:08:58 -0300

    ajustando funçoes básicas da calculadora

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,73 +1,159 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 
-function ButtonCalc({ title }) {
+function ButtonCalc({ title, onPress }) {
   return (
-    <TouchableOpacity style={[styles.button]}>
+    <TouchableOpacity style={[styles.button]} onPress={onPress}>
       <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
   );
 }
 
-function ButtonBlue({ title }) {
+function ButtonBlue({ title, onPress }) {
   return (
-    <TouchableOpacity style={[styles.button, styles.blue]}>
+    <TouchableOpacity style={[styles.button, styles.blue]} onPress={onPress}>
       <Text style={styles.textwhite}>{title}</Text>
     </TouchableOpacity>
   );
 }
 
-function ButtonGray({ title }) {
+function ButtonGray({ title, onPress }) {
   return (
-    <TouchableOpacity style={[styles.button, styles.gray]}>
+    <TouchableOpacity style={[styles.button, styles.gray]} onPress={onPress}>
       <Text style={styles.textwhite}>{title}</Text>
     </TouchableOpacity>
   );
 }
-
 
 export default function RootLayout() {
+  const [currentValue, setCurrentValue] = useState('0');
+  const [operator, setOperator] = useState(null);
+  const [previousValue, setPreviousValue] = useState(null);
+
+  function handleBackspace() {
+    if (currentValue.length === 1) {
+      setCurrentValue('0');
+      return;
+    }
+
+    const newValue = currentValue.slice(0, -1);
+
+    if (newValue === '-' || newValue === '') {
+      setCurrentValue('0');
+    } else {
+      setCurrentValue(newValue);
+    }
+  }
+
+  function handleNumber(value) {
+    if (value === '.' && currentValue.includes('.')) return;
+
+    if(currentValue ==='0'){
+      setCurrentValue(value);
+    }else{
+      setCurrentValue(currentValue + value);
+    }
+  }
+
+  function handleOperator(op) {
+    setOperator(op);
+    setPreviousValue(currentValue);
+    setCurrentValue('0');
+  }
+
+  function handleCalculate() {
+    const prev = parseFloat(previousValue);
+    const curr = parseFloat(currentValue);
+    let result = 0;
+
+    switch (operator) {
+      case '+':
+        result = prev + curr;
+        break;
+      case '-':
+        result = prev - curr;
+        break;
+      case 'X':
+        result = prev * curr;
+        break;
+      case '÷':
+        result = prev / curr;
+        break;
+      default:
+        return;
+    }
+
+    setCurrentValue(String(result));
+    setOperator(null);
+    setPreviousValue(null);
+  }
+
+  function handleClear() {
+    setCurrentValue('0');
+    setPreviousValue(null);
+    setOperator(null);
+  }
+
+  function handleToggleSign() {
+    const numero = parseFloat(currentValue);
+    const invertido = numero * -1;
+
+    setCurrentValue(invertido.toString());
+
+  }
+
+  function handleSqrt() {
+    const value = parseFloat(currentValue);
+    if (value < 0) return;
+    setCurrentValue(String(Math.sqrt(value)));
+  }
+
   return (
+
     <View style={styles.main}>
 
       <View>
-        <Text>Resultado:</Text>
+        <TextInput
+          style={styles.input}
+          value={currentValue}
+          editable={false} />
       </View>
 
       <View style={styles.content}>
         <View style={styles.row}>
-          <ButtonGray title="C" type="dark" />
-          <ButtonGray title="+-" type="dark" />
-          <ButtonGray title="%" type="dark" />
-          <ButtonBlue title="÷" />
+          <ButtonGray title="C" onPress={handleClear} />
+          <ButtonGray title="+-" onPress={handleToggleSign} />
+          <ButtonGray title="√" onPress={handleSqrt} />
+          <ButtonBlue title="÷" onPress={() => handleOperator('÷')} />
         </View>
 
         <View style={styles.row}>
-          <ButtonCalc title="7" />
-          <ButtonCalc title="8" />
-          <ButtonCalc title="9" />
-          <ButtonBlue title="X" />
+          <ButtonCalc title="7" onPress={() => handleNumber('7')} />
+          <ButtonCalc title="8" onPress={() => handleNumber('8')} />
+          <ButtonCalc title="9" onPress={() => handleNumber('9')} />
+          <ButtonBlue title="X" onPress={() => handleOperator('X')} />
         </View>
 
         <View style={styles.row}>
-          <ButtonCalc title="4" />
-          <ButtonCalc title="5" />
-          <ButtonCalc title="6" />
-          <ButtonBlue title="-" />
+          <ButtonCalc title="4" onPress={() => handleNumber('4')} />
+          <ButtonCalc title="5" onPress={() => handleNumber('5')} />
+          <ButtonCalc title="6" onPress={() => handleNumber('6')} />
+          <ButtonBlue title="-" onPress={() => handleOperator('-')} />
         </View>
 
         <View style={styles.row}>
-          <ButtonCalc title="1" />
-          <ButtonCalc title="2" />
-          <ButtonCalc title="3" />
-          <ButtonBlue title="+" />
+          <ButtonCalc title="1" onPress={() => handleNumber('1')} />
+          <ButtonCalc title="2" onPress={() => handleNumber('2')} />
+          <ButtonCalc title="3" onPress={() => handleNumber('3')} />
+          <ButtonBlue title="+" onPress={() => handleOperator('+')} />
         </View>
 
         <View style={styles.row}>
-          <ButtonCalc title="." />
-          <ButtonCalc title="0" />
-          <ButtonCalc title="⌫" />
-          <ButtonBlue title="=" />
+          <ButtonCalc title="." onPress={() => handleNumber('.')} />
+          <ButtonCalc title="0" onPress={() => handleNumber('0')} />
+          <ButtonCalc title="⌫" onPress={handleBackspace} />
+          <ButtonBlue title="=" onPress={handleCalculate} />
         </View>
       </View>
     </View>
@@ -116,9 +202,18 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "black",
   },
-    textwhite: {
+  textwhite: {
     fontSize: 20,
     fontWeight: "bold",
     color: "white",
+  },
+  input: {
+    backgroundColor: 'white',
+    fontSize: 40,
+    width: 300,
+    textAlign: 'right',
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 8
   }
 });
